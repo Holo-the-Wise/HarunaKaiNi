7e68af35f0316e324dbb43d5281fc8559f1ec51b
HasithaAbeysekera <hasithaa@hotmail.com>
2019-05-08 12:17:04 +0800

    username on leave message

---
+++ commands/admin/dsay.js
@@ -0,0 +1,43 @@
+ const {Command} = require('discord.js-commando')
+const generalChannelID = require("../../config.json").generalChannel;
+
+module.exports = class MuteCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'dsay',
+            memberName: 'dsay',
+            group: 'admin',
+            description: 'Haruna will talk in the mentioned channel, default if no channel is mentioned',
+            aliases: [],
+            examples: ['dsay2 [channel] [message]'],
+            format: 'dsay2 [channel] [message]',
+            args: [{
+                    key: 'dsaytext',
+                    prompt: 'Please type a message',
+                    type: 'string',
+                }
+                // ,
+                // {
+                //     key: 'textchannel',
+                //     prompt: 'Please enter a channel',
+                //     type: 'channel',
+                //     default: ''
+                // }
+            ],
+            guildOnly: false,
+            ownerOnly: true
+        })
+    }
+
+    hasPermission(message) {
+        let PermissionLevel = 3;
+        let msglevel = message.client.elevation(message);
+        return msglevel >= PermissionLevel;
+    }
+
+    async run(message, {dsaytext}) {
+        console.log("hi0");
+        message.say(dsaytext);
+        
+    }
+};
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,6 +3,6 @@ const logChannelID = require("../config.json").logChannel;
 module.exports = async (client, member) => {
     let guild = member.guild;
     logChannel = guild.channels.find(u => u.id == logChannelID);
-    console.log(`${member.user.username} has left ${guild}`);
-    logChannel.send(`${member.user} has left the server :cry:`);
+    console.log(`${member.user.username}  has left ${guild}`);
+    logChannel.send(`${member.user} (${member.user.username}) has left the server :cry:`);
 };
