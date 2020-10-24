149635f8e3948cd02a316857925ed5fac102bf5e
Hasitha Abeysekera <hasithaa@hotmail.com>
2020-10-24 11:50:33 +0800

    fixed dsay command

--- commands/admin/dsay.js
+++ commands/admin/dsay.js
@@ -1,27 +1,30 @@
-const {Command} = require('discord.js-commando');
+const { Command } = require('discord.js-commando')
 const logger = require('../../util/logging');
 
-module.exports = class MuteCommand extends Command {
-    constructor(client) {
+module.exports = class DsayCommand extends Command {
+    constructor (client) {
         super(client, {
             name: 'dsay',
             memberName: 'dsay',
-            group: 'admin',
-            description: 'Haruna will talk in the mentioned channel. Server only command',
-            format: '[channel] [message]',
+            group: 'admin', 
+            description: 'Haruna will talk in the mentioned channel. Server only command', 
+            format: '[channel] [message]', 
             examples: ['dsay2 #general hello there'],
-            args: [{
-                key: 'textchannel',
-                prompt: 'Please enter a channel',
-                type: 'channel',
-            },
-            {
-                key: 'dsaytext',
-                prompt: 'Please type a message',
-                type: 'string',
-            }],
-            defaultHandling: false,
-            guildOnly: true,
+            args: [
+                {
+                    key: 'textchannel',
+                    prompt: 'Please enter a channel',
+                    type: 'channel',
+                },
+                {
+                    key: 'dsaytext',
+                    prompt: 'Please type a message',
+                    type: 'string',
+                }
+            ],
+            argsPromptLimit: 2,  
+            defaultHandling: true, 
+            guildOnly: true,   
         })
     }
 
@@ -30,9 +33,8 @@ module.exports = class MuteCommand extends Command {
         let msglevel = message.client.elevation(message);
         return msglevel >= PermissionLevel;
     }
-
-    async run(message, {textchannel, dsaytext}) {
-
+    
+    async run (message, {textchannel, dsaytext}) {
         if(textchannel.type != 'text'){
             return message.channel.send(`Destination channel must be a text channel`).then(message.delete());
         } else {
