9fb423a7f63564faf131e97a5a9d655d6e8db929
Hayashion <66824311+Hayashion@users.noreply.github.com>
2021-07-15 19:37:39 +0800

    Twitterfix - Suppresses Previous Embed

--- events/message.js
+++ events/message.js
@@ -5,14 +5,13 @@ module.exports = async (client, message) => {
     }
     if (message.content.includes("https://twitter.com")) {
         const fetch = require("node-fetch");
-
-        var tweetLinkRegex = new RegExp(/https:\/\/twitter.com\/\w*\/status\/\d*/);
-        var tweetLink = message.content.match(tweetLinkRegex)[0];
+        const tweetLinkRegex = new RegExp(/https:\/\/twitter.com\/\w*\/status\/\d*/);
+        const tweetLink = message.content.match(tweetLinkRegex)[0];
         tweetId = tweetLink.split('/').reverse()[0]
 
         const tweetUrl = `https://api.twitter.com/2/tweets/${tweetId}?expansions=attachments.media_keys&media.fields=type`
 
-        var obj = {
+        const obj = {
             method: 'GET',
             headers: {
                 'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAABCIRgEAAAAAt8VpNcbLbZgPyhkL3Tvlo%2FOsrCg%3DrR4bOGRHuu2ymmAnYQK0XLSlvN0ShzYPySZla8FbliMg3qW7M5',
@@ -20,12 +19,23 @@ module.exports = async (client, message) => {
             },
         };
 
+        const obj2 = {
+            method: 'PATCH',
+            headers: {
+                'Authorization': 'Bot NTM2ODQ2OTEzMzk2NDA4MzMw.XEWXpg.AuMEhzEZxxrzZOL1DTU6CajilbA',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                'flags': 4
+            }),
+        };
+
         // Gets Tweet Info, Checks if it is a Video/GIF
         fetch(tweetUrl, obj)
             .then((resp) => resp.json())
             .then(function (data) {
                 // console.log(data.includes.media[0].type);
-                var mediaType = data.includes.media[0].type;
+                const mediaType = data.includes.media[0].type; ``
                 if (mediaType == 'video' || mediaType == 'animated_gif') {
 
                     var messageContent = message.content;
@@ -36,35 +46,28 @@ module.exports = async (client, message) => {
                     const avatar = member.user.displayAvatarURL({ format: 'jpg', dynamic: true })
                     const name = member.displayName;
                     // console.log('stuff',name,avatar)
-
+                    // message.suppressEmbeds()
+                    //     .catch(err => console.log(err))
+                    // console.log(`https://discord.com/api/v8/channels/${message.channel.id}/messages/${message.id}`)
+                    fetch(`https://discord.com/api/channels/${message.channel.id}/messages/${message.id}`, obj2)
+                        .catch(err => console.log(err));
                     guild.fetchWebhooks()
-                        .then(function(webhooks) {
+                        .then(function (webhooks) {
                             var fxTwitter = webhooks.find(webhook => webhook.name == 'fxtwitter');
                             return fxTwitter;
                         })
-                        .then(fxTwitter => fxTwitter.edit({channel:channelId}))
-                        .then(fxTwitter => fxTwitter.send(messageContent.replace(tweetLink,fxTweetLink),{username:name,avatarURL:avatar}))
+                        .then(fxTwitter => fxTwitter.edit({ channel: channelId }))
+                        .then(fxTwitter => fxTwitter.send(messageContent.replace(tweetLink, fxTweetLink), { username: name, avatarURL: avatar }))
+                        // .then(message.delete())
+                        // .then(message.edit({flags: ['SUPPRESS_EMBEDS']}))
+                        // .then(resp => console.log(resp))
                         .catch((error) => console.log(error));
-                        
-                    
 
-
-                    // message.say(messageContent.replace(tweetLink,fxTweetLink)) // delete after
-                    message.delete()
-                        .then()
-                        .catch((error) => console.log(error));
+                    console.log("TwitterFix applied to " + name)
                 };
             })
-            .catch(function (error) {
-                console.log(error)
-            });
-
-
-        // Gets message information in prep for webhook
-
-        // Creats a new webhook and posts message with fixed url then deletes original msg
-
-
+            .catch(error => console.log(error));
+        return;
     }
     if (!message.content.startsWith(client.commandPrefix)) {
         return;
